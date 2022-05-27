3c8602d8a76dcc37574736df91372a1425093c27
Sergey Grigorev <grigorevs102@gmail.com>
2022-05-27 16:34:34 +0300

    some changes in chats

--- src/components/MessageItem.jsx
+++ src/components/MessageItem.jsx
@@ -5,10 +5,11 @@ const MessageItem = (props) => {
   return (
     <div>
       {props.dispMessage.name}: {props.dispMessage.text}&nbsp;
-      <Likes />
+      {props.flag === "dispute" ? <Likes /> : null}
       <div>
         {props.dispMessage.date.getHours()}:
-        {props.dispMessage.date.getMinutes()}
+        {(props.dispMessage.date.getMinutes() < 10 ? "0" : "") +
+          props.dispMessage.date.getMinutes()}
       </div>
     </div>
   );
